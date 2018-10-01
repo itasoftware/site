a146c2174596448475f503f257e92e2deb1e029d
itasoftware <43734259+itasoftware@users.noreply.github.com>
2018-09-30 22:07:16 -0300

    Add files via upload

---
+++ indexold.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>Bem vindo ao site da ItaSoftware</title>
+<style type="text/css">
+.f {
+	font-family: Verdana, Geneva, sans-serif;
+	text-align: center;
+}
+</style>
+</head>
+
+<body>
+<table width="200" border="0" align="center">
+  <tr>
+    <td><img src="logo_site.jpg" width="828" height="388" /></td>
+  </tr>
+</table>
+<p class="f">Estamos em Manutenção</p>
+<p class="f">contato@itasoftware.com.br</p>
+</body>
+</html>
